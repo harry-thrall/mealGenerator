f0878a5d9a581256d3511a61465c09f185c20fa2
harry-thrall <52261914+harry-thrall@users.noreply.github.com>
2020-03-26 00:40:03 +0000

    Upload of initial website for meal generation

---
+++ script.js
@@ -0,0 +1,70 @@
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+get_meal_btn.addEventListener('click', () => {
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(response => {
+      createMeal(res.meals[0]);
+    })
+    .catch(e => {
+      console.warn(e);
+    });
+});
+
+const createMeal = meal => {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++){
+    if(meal[`strIngredients${i}`]){
+      ingredients.push(
+        `${meal[`strIngredients${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      //No more ingredients, stop running
+      break;
+    }
+  }
+
+  const newInnerHTML = `
+    <div class = "row">
+      <div class = "columns five">
+        <img src="${meal.strMealThumb}" alt = "Image of meal">
+        ${
+          meal.strCategory
+            ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+            : ''
+        }
+        ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+        ${
+          meal.strTags
+            ? `<p><strong>Tags:</strong> ${meal.strTags
+                              .split(',')
+                              .join(', ')}</p>`
+            : ''
+        }
+        <h5>Ingredients:</h5>
+        <ul>
+          ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+        </ul>
+      </div>
+      <div class="column seven">
+        <h4>${meal.strMeal}</h4>
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+    ${
+      meal.strYoutube
+        ? `
+          <div class = "row">
+            <h5>Video Recipe</h5>
+            <div class = "videoWrapper">
+              <iframe width = "420" height = "315" src = "https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}"></iframe>
+            </div>
+          </div>`
+            : ''
+    }
+  `;
+
+  meal_container.innerHTML = newInnerHTML;
+};
